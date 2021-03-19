66f4a50726d584025244247db86beb10eb54addc
jachtomadorota <jachtoma.do@gmail.com>
2021-03-19 18:15:01 +0100

    first initla commit

---
+++ WiredBrainCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.dorotajachtoma.wiredbraincoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiredBrainCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiredBrainCoffeeApplication.class, args);
+	}
+
+}
