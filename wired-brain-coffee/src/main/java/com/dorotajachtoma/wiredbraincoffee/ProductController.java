aa7f5340aa3d78f70b3f548c016ffc1fe19fbda8
jachtomadorota <jachtoma.do@gmail.com>
2021-03-22 14:00:39 +0100

    add all crud operations

---
+++ wired-brain-coffee/src/main/java/com/dorotajachtoma/wiredbraincoffee/ProductController.java
@@ -0,0 +1,59 @@
+package com.dorotajachtoma.wiredbraincoffee;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    public final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Flux<Product> getAllProducts(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Product>> getOneProduct(@PathVariable String id){
+        return repository.findById(id)
+                .map(product -> ResponseEntity.ok(product));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> saveProduct(@RequestBody Product product){
+        return repository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Product>> updateProduct(@PathVariable(value = "id") String id,
+                                                       @RequestBody Product product){
+        return repository.findById(id)
+                .flatMap(existingProduct ->{
+                    existingProduct.setPrice(product.getPrice());
+                    existingProduct.setName(product.getName());
+                    return repository.save(existingProduct);
+                })
+                .map(updateProduct -> ResponseEntity.ok(updateProduct))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable(value = "id") String id){
+        return repository.findById(id)
+                .flatMap(existingProduct ->
+                        repository.delete(existingProduct)
+                .then(Mono.just(ResponseEntity.ok().<Void>build())));
+    }
+
+
+}
